732f20df916c416ecc9fc9d0e22e76ed51ead392
Brian Dawn <brian.t.dawn@gmail.com>
2016-12-10 16:01:11 -0600

    Problem 6, part 2.

--- src/advent_of_code/problem_6.clj
+++ src/advent_of_code/problem_6.clj
@@ -4,10 +4,18 @@
 
 (def data (clojure.string/split-lines input))
 
+(println "part 1"
+         (->> (apply map vector data)
+              (map frequencies)
+              (map (comp reverse (partial sort-by val)))
+              (map first)
+              (map first)
+              (apply str)))
 
-(->> (apply map vector data)
-     (map frequencies)
-     (map (comp reverse (partial sort-by val)))
-     (map first)
-     (map first)
-     (apply str))
+(println "part 2"
+         (->> (apply map vector data)
+              (map frequencies)
+              (map (partial sort-by val))
+              (map first)
+              (map first)
+              (apply str)))
