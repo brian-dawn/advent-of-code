4f5fc26a6d0157fa7fb811362e410c54091ae240
Brian Dawn <brian.t.dawn@gmail.com>
2016-12-10 17:12:44 -0600

    Problem 7, part 1.

---
+++ src/advent_of_code/problem_7.clj
@@ -0,0 +1,45 @@
+(ns advent-of-code.problem-7)
+
+(defn abba? [xs]
+  (let [[a b c d] xs]
+    (and (= a d)
+         (= b c)
+         (not= a b)
+         (= 4 (count xs)))))
+
+(def input (slurp "day-7.txt"))
+
+(def data (clojure.string/split-lines input))
+
+
+
+(count
+ (filter
+  (fn [ip]
+    (let [clean (partial filter (comp abba? first))
+          inside-brackets  (->> ip
+                                (re-seq #"\[.*?]")
+                                (map rest)
+                                (map butlast)
+                                (map (partial apply str))
+                                )
+          
+          outside-brackets (clojure.string/split ip #"\[.*?\]")] 
+
+      (and
+       (->> outside-brackets
+            (map (partial partition 4 1))
+            (apply concat)
+            (some abba?)) 
+       
+       (->> inside-brackets
+            (map (partial partition 4 1))
+            (apply concat)
+            (not-any? abba?)))))
+
+  data))
+
+
+
+
+
