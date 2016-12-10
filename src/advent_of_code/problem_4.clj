9f5fbee241b1da39dbeb69ed3b28e70e8c184b4f
Brian Dawn <brian.t.dawn@gmail.com>
2016-12-10 15:34:14 -0600

    Problem 4, part 2.

--- src/advent_of_code/problem_4.clj
+++ src/advent_of_code/problem_4.clj
@@ -10,28 +10,53 @@
                 :sector-id (Integer/parseInt sector-id)
                 :chksum chksum}))))
 
-(->
- (fn [acc {:keys [chksum data sector-id]}]
 
-   (let [calculated-chksum
-         (->> data
-              (apply concat)
-              frequencies
-              (sort-by val)
-              reverse
-              ;; Sort same count alphabetically
-              (partition-by second)
-              (map (partial sort-by first))
-              ;; Get just the keys.
-              (map (partial keys))
-              flatten
-              (apply str))]
-     
-     (if (.startsWith calculated-chksum chksum)
-       (+ acc sector-id)
-       acc)))
- 
- (reduce 0 data))
+
+(def alphabet (->> (range (int \a) (inc (int \z)))
+                   (map char)
+                   cycle))
+
+(defn shift [amount s]
+  (->> s
+       (map (fn [ch]
+              (->> alphabet
+                   (drop-while (partial not= ch))
+                   (drop amount)
+                   first)))
+       (apply str)))
+
+(let [[acc decryped-messages]
+      (->
+       (fn [[acc decrypted] {:keys [chksum data sector-id]}]
+
+         (let [calculated-chksum
+               (->> data
+                    (apply concat)
+                    frequencies
+                    (sort-by val)
+                    reverse
+                    ;; Sort same count alphabetically
+                    (partition-by second)
+                    (map (partial sort-by first))
+                    ;; Get just the keys.
+                    (map (partial keys))
+                    flatten
+                    (apply str))]
+           
+           (if (.startsWith calculated-chksum chksum)
+             [(+ acc sector-id)
+              (conj decrypted [sector-id (clojure.string/join " " (map (partial shift sector-id) data))])]
+             
+             [acc decrypted])))
+       
+       (reduce [0 #{}] data))]
+  (println "part 1" acc)
+  (doseq [[sector-id msg] decryped-messages]
+    (when (.contains msg "north")
+      (println sector-id ":" msg))))
+
+
+
 
 
 
