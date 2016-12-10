fcee5e4943ccf9495cd74752b29dc76f95ee0c3a
Brian Dawn <brian.t.dawn@gmail.com>
2016-12-10 15:02:48 -0600

    Problem 3, part 2.

--- src/advent_of_code/problem_3.clj
+++ src/advent_of_code/problem_3.clj
@@ -6,11 +6,28 @@
                     (re-seq #"(\d+)\s+(\d+)\s+(\d+)")
                     (map (fn [match-data]
                            (map #(Integer/parseInt %) (rest match-data))))))
+(println "part 1"
+       (count (filter
+               (fn [[a b c]]
+                 (and
+                  (< a (+ b c))
+                  (< b (+ a c))
+                  (< c (+ a b))))
+               triangles)))
 
-(count (filter
-        (fn [[a b c]]
-          (and
-           (< a (+ b c))
-           (< b (+ a c))
-           (< c (+ a b))))
-        triangles))
+(println "part 2"
+         (->> 
+          (for [i (range 3)]
+            (map #(nth % i) triangles))
+          flatten
+          (partition 3)
+          (filter (fn [[a b c]]
+                    (and
+                     (< a (+ b c))
+                     (< b (+ a c))
+                     (< c (+ a b)))))
+          count))
+
+
+
+
