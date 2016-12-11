a7876b859b4c1a8948dac108495841a058979edf
Brian Dawn <brian.t.dawn@gmail.com>
2016-12-10 20:35:41 -0600

    add gitignore

--- src/advent_of_code/problem_7.clj
+++ src/advent_of_code/problem_7.clj
@@ -43,3 +43,38 @@
 
 
 
+;; part 2
+
+(defn aba? [[a b c :as xs]]
+  (and
+   (= a c)
+   (not= a b)
+   (= 3 (count xs))))
+
+
+
+(count
+ (filter
+  (fn [ip]
+    (let [clean (partial filter (comp abba? first))
+          inside-brackets  (->> ip
+                                (re-seq #"\[.*?]")
+                                (map rest)
+                                (map butlast)
+                                (map (partial apply str))
+                                )
+          
+          outside-brackets (clojure.string/split ip #"\[.*?\]")] 
+
+      (and
+       (->> outside-brackets
+            (map (partial partition 4 1))
+            (apply concat)
+            (some abba?)) 
+       
+       (->> inside-brackets
+            (map (partial partition 4 1))
+            (apply concat)
+            (not-any? abba?)))))
+
+  data))
